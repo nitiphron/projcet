7d5df20aa939458cb52e774b8c8150c200d1907b
nitiphron <hardandmin@gmail.com>
2024-03-01 07:43:42 +0700

    Initial commit

---
+++ prisma/seed.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcryptjs')
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+const password = bcrypt.hashSync('123456')
+const userData = [
+    {
+    username : 'min',
+    password : password,
+    email: 'min@gmail.com',
+    first_name : 'nitiphon',
+    last_name : 'chaisue',
+    address : '156/10',
+    phone : 654743544
+  },
+
+  {
+    username : 'hard',
+    password : password,
+    email: 'hard@gmail.com',
+    first_name : 'nit',
+    last_name : 'c',
+    address : '156/10',
+    phone : 896546315
+  }
+    
+]
+
+const todoData = [
+  { title:'Learn HTML', dueDate: new Date(), userId: 1 },
+  { title:'Learn CSS', dueDate: new Date(), userId: 2 },
+  { title:'Learn JS', dueDate: new Date(), userId: 3 },
+  { title:'Learn React', dueDate: new Date(), userId: 4 },
+
+]
+
+
+
+const run = async () => {
+  await prisma.user.createMany({
+    data : userData
+  })
+  await prisma.todo.createMany({
+    data : todoData
+  })
+}
+
+run()
